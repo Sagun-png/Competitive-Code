c0479fec6dba29e1f80b615fb0840a7f980efd76
Sagun-png <67581440+Sagun-png@users.noreply.github.com>
2021-06-01 14:11:59 +0530

    Update PallindromeGame(Easy) - CF.cpp

--- PallindromeGame(Easy) - CF.cpp
+++ PallindromeGame(Easy) - CF.cpp
@@ -9,12 +9,9 @@ int main()
         int n; string s;
         cin>>n;
         cin>>s;
-        if(s.length()==1) {cout<<"BOB"<<endl; continue;}
         int cnt[2]={0};
         for(int i=0;i<s.length();i++)
             cnt[s[i]-48]++;
-        if(cnt[1]==0 && cnt[0]%2!=0) {cout<<"ALICE"<<endl; continue;}
-        if(cnt[1]==0 && cnt[0]%2==0) {cout<<"BOB"<<endl; continue;}
         if(cnt[0]==1) {cout<<"BOB"<<endl; continue;}
         if(cnt[0]%2==0) {cout<<"BOB"<<endl; continue;}
         if(cnt[0]%2!=0) {cout<<"ALICE"<<endl; continue;}
