61bbb1fb0947b6e5d5814f0e7de9f3423be4c9e7
Sagun-png <67581440+Sagun-png@users.noreply.github.com>
2021-07-11 08:34:55 +0530

    Create CustomisingTheTrack.cpp

---
+++ CustomisingTheTrack.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long int a,b,res1,cnt=0;
+        cin>>a>>b;
+        res1=abs(b-a);
+        cout<<res1<<" ";
+        if(a==b) cout<<"0"<<"\n";
+        else if(__gcd(a, b)==res1) cout<<"0"<<"\n";
+        else{
+            if(a%res1>res1/2)
+                cout<<res1-(a%res1)<<"\n";
+            else
+                cout<<a%res1<<"\n";
+        }
+    }
+}
