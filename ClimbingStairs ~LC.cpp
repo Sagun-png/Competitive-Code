cb091fc77a99f876ebc620759e56852848d9d976
Sagun-png <67581440+Sagun-png@users.noreply.github.com>
2021-07-12 15:41:40 +0530

    Create ClimbingStairs ~LC.cpp

---
+++ ClimbingStairs ~LC.cpp
@@ -0,0 +1,19 @@
+int climb_Stairs(int i, int n, int memo[]) {
+        if (i > n) {
+            return 0;
+        }
+        if (i == n) {
+            return 1;
+        }
+        if (memo[i] > 0) {
+            return memo[i];
+        }
+        memo[i] = climb_Stairs(i + 1, n, memo) + climb_Stairs(i + 2, n, memo);
+        return memo[i];
+    }
+    
+    int climbStairs(int n) {
+        int memo[45];
+        memset(memo,0,sizeof(memo));
+        return climb_Stairs(0,n,memo);
+    }
