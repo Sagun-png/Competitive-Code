d31f5bcd99d10b674271e27f77d57e49501a6808
Sagun-png <67581440+Sagun-png@users.noreply.github.com>
2021-07-18 12:14:00 +0530

    Create Alphabetical Strings.cpp

---
+++ Codeforces/800/Alphabetical Strings.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+		int len=s.length();
+		int l=0,r=len-1;
+		for(int i=len;i;i--){
+			if(s[l]=='a'+i-1) l++;
+			else if(s[r]=='a'+i-1) r--;
+			else break;
+		}
+		if(l<=r) puts("NO");
+		else puts("YES");
+    }
+}
