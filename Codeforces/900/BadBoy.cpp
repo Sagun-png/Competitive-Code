f0f7271c70632f7eaf778698d007d26c853b627c
Sagun-png <67581440+Sagun-png@users.noreply.github.com>
2021-07-18 12:23:10 +0530

    Create BadBoy.cpp

---
+++ Codeforces/900/BadBoy.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    while (t--){
+        int n, m, x, y;
+        cin >> n >> m >> x >> y;
+ 
+        cout << 1 << ' ' << 1 << ' '<< n << ' ' << m << '\n';
+    }
+    return 0;
+}
