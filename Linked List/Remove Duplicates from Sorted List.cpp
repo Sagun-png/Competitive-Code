f6a09d65fb2f078fa962a160797af695830e1e9a
Sagun-png <67581440+Sagun-png@users.noreply.github.com>
2021-08-30 09:54:04 +0530

    Create Remove Duplicates from Sorted List.cpp

---
+++ Linked List/Remove Duplicates from Sorted List.cpp
@@ -0,0 +1,26 @@
+ListNode* deleteDuplicates(ListNode* head) {
+        map<int,int> mp;
+        ListNode* t;
+        ListNode* ans=new ListNode();
+        ans=NULL;
+        t=head;
+        while(t!=NULL){
+            if(mp[t->val]!=1)
+                mp[t->val]=1;
+            t=t->next;
+        }
+        for(auto it=mp.rbegin();it!=mp.rend();++it){
+            if(it->second==1){
+                ListNode* tp=new ListNode();
+                tp->val = it->first;
+                tp->next==NULL;
+                if(ans==NULL){
+                    ans=tp;
+                    continue;
+                }
+                tp->next=ans;
+                ans=tp;
+            }
+        }
+        return ans;
+    }
